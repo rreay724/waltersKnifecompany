8caeb5b5cf8e0eea510ded6484841584ab73a975
Bobby Reay <rreay724@gmail.com>
2021-03-18 09:43:31 -0400

    added carousel

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,37 @@
 import React from "react";
+import "./home.css";
+import { Carousel } from "react-responsive-carousel";
+
+import image1 from "../../images/knife1.jpeg";
+import image2 from "../../images/knife2.jpeg";
+import image3 from "../../images/knife3.jpeg";
+import image4 from "../../images/knife4.jpeg";
 
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="home">
+      <div className="homeCarousel">
+        <div className="homeTop">
+          <Carousel
+            autoPlay={true}
+            infiniteLoop={true}
+            interval={4000}
+            centerMode={true}
+            centerSlidePercentage={60}
+            showThumbs={false}
+          >
+            <div>
+              <img src={image1} />
+            </div>
+            <div>
+              <img src={image2} />
+            </div>
+            <div>
+              <img src={image3} />
+            </div>
+          </Carousel>
+        </div>
+      </div>
     </div>
   );
 }
